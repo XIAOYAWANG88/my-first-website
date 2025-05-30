3568a6f28ec37dcf13fcc4142f69c0870a7ee745
XIAOYAWANG88 <xiaoyaw6@student.unimelb.edu.au>
2025-05-31 06:01:05 +1000

    The homepage adds a search function, which will pop up related searches

--- js/common.js
+++ js/common.js
@@ -2,14 +2,13 @@ const searchInput = document.querySelector('.search-box input');
 const searchDropdown = document.getElementById('searchDropdown');
 
 searchInput.addEventListener('input', (e) => {
-  if (e.target.value === 'cat food') {
+  if (e.target.value) {
     searchDropdown.style.display = 'flex';
   } else {
     setTimeout(() => { // 延迟隐藏，避免点击下拉内容时立刻消失
       searchDropdown.style.display = 'none';
     }, 150);
   }
- 
 });
 searchInput.addEventListener('blur', () => {
   setTimeout(() => { // 延迟隐藏，避免点击下拉内容时立刻消失
@@ -21,19 +20,19 @@ const cartIcon = document.querySelector('#icon-cart-menu');
 const cartPopup = document.getElementById('cart-popup');
 
 // 购物车图标和弹窗的显示/隐藏
-cartIcon.addEventListener('mouseenter', function() {
+cartIcon && cartIcon.addEventListener('mouseenter', function() {
   cartPopup.style.display = 'block';
 });
-cartIcon.addEventListener('mouseleave', function() {
+cartIcon && cartIcon.addEventListener('mouseleave', function() {
   setTimeout(() => {
     if (!cartPopup.matches(':hover')) {
       cartPopup.style.display = 'none';
     }
   }, 200);
 });
-cartPopup.addEventListener('mouseleave', function() {
+cartPopup && cartPopup.addEventListener('mouseleave', function() {
   cartPopup.style.display = 'none';
 });
-cartPopup.addEventListener('mouseenter', function() {
+cartPopup && cartPopup.addEventListener('mouseenter', function() {
   cartPopup.style.display = 'block';
 });
